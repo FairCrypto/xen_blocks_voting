9bbf7a3dd3f6a336888671814f945a8774a9767c
Leonid Belyaev <lbeliaev@gmail.com>
2024-11-11 08:43:06 -0800

    fixed period generation function

--- app/test_db.ts
+++ app/test_db.ts
@@ -1,10 +1,10 @@
-import {getAllVoters, getVoterInfo, initDB, insertVoterCreditRecord} from "../db/db";
+import {initDB} from "../db/db";
 
 async function main() {
     await initDB().then(() => console.log('db initialized'));
-    await getVoterInfo('FwznmVkDsvf3v56Y4vV8MtGUNQ8PJGUVMLT5demygHwV').then(console.log);
+    // await getVoterInfo('FwznmVkDsvf3v56Y4vV8MtGUNQ8PJGUVMLT5demygHwV').then(console.log);
     console.log("\n\n");
-    await getAllVoters().then(console.log);
+    // await getAllVoters().then(console.log);
 }
 
 main().catch(console.error)
