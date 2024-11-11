39148170dc9e081a8d36d1689a71dd12c594c1a5
Leonid Belyaev <lbeliaev@gmail.com>
2024-11-11 09:00:52 -0800

    added pm2 config

---
+++ pm2/ecosystem.config.js
@@ -0,0 +1,43 @@
+module.exports = {
+    apps: [
+        {
+            name: "webserver",
+            cwd: "../grow_space",
+            script: "./app/server_vote_receiver.js",
+            args: "--max-old-space-size=512 --expose-gc",
+            mode: "cluster",
+
+            env: {
+                NODE_ENV: "production",
+                ANCHOR_WALLET: "/mnt/ledger/build/.config/solana/id.json",
+                ANCHOR_PROVIDER_URL: "https://xolana-devnet.xen.network"
+            }
+        },
+        {
+            name: "voter_crediter",
+            script: "./app/server_vote_receiver.js",
+            args: "--max-old-space-size=512 --expose-gc",
+            env: {
+                NODE_ENV: "production",
+                DB_LOCATION: './.db',
+                ANCHOR_PROVIDER_URL: "https://xolana-devnet.xen.network"
+            }
+        },
+        {
+            name: "create_periods",
+            script: "./app/create_periods.js",
+            env: {
+                NODE_ENV: "production",
+                DB_LOCATION: './.db',
+            }
+        },
+        {
+            name: "allocate_rewards",
+            script: "./app/allocate_rewards.js",
+            env: {
+                NODE_ENV: "production",
+                DB_LOCATION: './.db',
+            }
+        },
+    ]
+};
