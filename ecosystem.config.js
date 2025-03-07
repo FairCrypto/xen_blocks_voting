2903645ef61517af6c7118067e02d45589ee9941
Leonid Belyaev <lbeliaev@gmail.com>
2025-03-07 12:58:10 -0800

    added voters stats calc

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,21 +1,21 @@
 module.exports = {
     apps: [
         {
-            name: "webserver",
-            cwd: "../grow_space",
+            name: "voter_crediter",
             script: "./app/server_vote_receiver.js",
             interpreter_args: "--max-old-space-size=512 --expose-gc",
             // mode: "cluster",
             // instances: 4,
             env: {
                 NODE_ENV: "production",
+                DB_LOCATION: './.db',
                 ANCHOR_WALLET: "/home/ubuntu/.config/solana/voters_id.json",
                 ANCHOR_PROVIDER_URL: "https://xolana-devnet.xen.network"
             }
         },
         {
-            name: "voter_crediter",
-            script: "./app/server_vote_receiver.js",
+            name: "api",
+            script: "./app/api.js",
             interpreter_args: "--max-old-space-size=512 --expose-gc",
             env: {
                 NODE_ENV: "production",
