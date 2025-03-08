41168ef83a08fd385dadeb35976592c59ce28654
Leonid Belyaev <lbeliaev@gmail.com>
2025-03-07 16:02:55 -0800

    added voters stats calc

--- db/db.ts
+++ db/db.ts
@@ -266,7 +266,7 @@ const isPostgres = (process.env.DB_LOCATION || '').startsWith("postgres")
 export const fmt = (sql: string, isPsql: boolean = isPostgres) => {
     if (isPsql) return sql
         .replace(/DATETIMETZ/g, 'TIMESTAMPTZ')
-        .replace(/AUTOINCREMENT/g, 'SERIAL')
+        .replace(/INTEGER PRIMARY KEY AUTOINCREMENT/g, 'SERIAL PRIMARY KEY')
     return sql
 }
 
